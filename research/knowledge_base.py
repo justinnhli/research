131652a4b0757e7949812dd9d69175973a8d9e57
Justin Li <justinnhli@gmail.com>
2018-04-27 11:16:53 -0700

    move query classes into separate file

--- research/knowledge_base.py
+++ research/knowledge_base.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-import re
 from os.path import exists as file_exists, splitext as split_ext, expanduser, realpath
-from textwrap import indent, dedent
 
 from SPARQLWrapper import SPARQLWrapper2
 from rdflib import Graph, Literal, URIRef, plugin
-from rdflib.plugins.sparql import prepareQuery
 from rdflib.store import Store
 from rdflib.util import guess_format
 from rdflib_sqlalchemy import registerplugins
@@ -14,188 +11,47 @@ from rdflib_sqlalchemy import registerplugins
 registerplugins()
 
 
-class URI:
-    PREFIXES = {
-        '_': '_',
-        'db': 'http://dbpedia.org/',
-        'dbo': 'http://dbpedia.org/ontology/',
-        'dbp': 'http://dbpedia.org/property/',
-        'dbr': 'http://dbpedia.org/resource/',
-        'dc': 'http://purl.org/dc/elements/1.1/',
-        'foaf': 'http://xmlns.com/foaf/0.1/',
-        'owl': 'http://www.w3.org/2002/07/owl#',
-        'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-        'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
-        'res': 'http://www.w3.org/2005/sparql-results#',
-        'skos': 'http://www.w3.org/2004/02/skos/core#',
-        'xsd': 'http://www.w3.org/2001/XMLSchema#',
-        'umbel-rc': 'http://umbel.org/umbel/rc/',
-        'umbel': 'http://umbel.org/umbel#',
-    }
-    REVERSED = sorted(
-        ([namespace, prefix] for prefix, namespace in PREFIXES.items()),
-        key=(lambda kv: -len(kv[0])),
-    )
-
-    def __init__(self, uri, prefix=None):
-        if prefix:
-            self.prefix = prefix
-            self.fragment = uri
-            self.uri = URI.PREFIXES[prefix] + uri
-        else:
-            self.uri = uri
-            for namespace, alias in URI.REVERSED:
-                if uri.startswith(namespace):
-                    self.prefix = alias
-                    self.fragment = uri[len(namespace):]
-
-    def __str__(self):
-        return '<' + self.uri + '>'
-
-    @property
-    def short_str(self):
-        return self.prefix + ':' + self.fragment
-
-
-class Node:
-    IS_VARIABLE = False
-
-    def __init__(self, name, **props):
-        self.name = name
-        self.props = props
-
-    def to_triple_list(self):
-        my_triples = []
-        child_triples = []
-        for k, child in self.props.items():
-            if not isinstance(child, Node):
-                child = L(child)
-            prop = URI(*reversed(k.split('__', maxsplit=1)))
-            my_triples.append('{} {} {} .'.format(self, prop, child))
-            child_triples.extend(child.to_triple_list())
-        return my_triples + child_triples
-
-    def variables(self):
-        seen = set(self.name)
-        variables = []
-        if self.IS_VARIABLE:
-            variables.append(str(self))
-        for v in self.props.values():
-            if isinstance(v, str):
-                desc_vars = ['?{}'.format(v)]
-            else:
-                desc_vars = v.variables()
-            desc_vars = [dv for dv in desc_vars if dv not in seen]
-            seen |= set(desc_vars)
-            variables.extend(desc_vars)
-        return variables
-
-    @staticmethod
-    def from_str(string):
-        if string[0] == '<' and string[-1] == '>':
-            return U(string[1:-1])
-        elif string.startswith('"'):
-            while not (string[0] == '"' and string[-1] == '"'):
-                string = re.sub(r'^(".*")@[a-z]*$', r'\1', string)
-                string = re.sub(r'^(".*")\^\^[^"]*$', r'\1', string)
-            return L(string[1:-1])
-        else:
-            try:
-                return L(int(string))
-            except ValueError:
-                try:
-                    return L(float(string))
-                except ValueError:
-                    return U(*reversed(string.split(':', maxsplit=1)))
-        raise ValueError('unable to parse "{}"'.format(string))
-
-
-class V(Node):
-    IS_VARIABLE = True
-
-    def __str__(self):
-        return '?{}'.format(self.name)
-
-
-class U(Node):
-
-    def __init__(self, uri, prefix=None, **props):
-        self.uri = URI(uri, prefix)
-        super().__init__(self.uri.uri, **props)
-
-    def __str__(self):
-        return str(self.uri)
-
-    @property
-    def short_str(self):
-        return self.uri.short_str
-
-
-class L(Node):
-
-    def __init__(self, val):
-        super().__init__(val)
-        assert isinstance(val, (int, float, str))
-
-    def __str__(self):
-        if isinstance(self.name, str):
-            return '"{}"'.format(self.name.replace('"', '\\"'))
-        else:
-            return str(self.name)
-
-    def variables(self):
-        return []
-
-
-class Query:
-
-    def __init__(self, *paths):
-        self.paths = paths
-
-    def variables(self, bound_vars=None):
-        if bound_vars is None:
-            bound_vars = []
-        result = []
-        for path in self.paths:
-            for variable in path.variables():
-                if variable[1:] not in bound_vars:
-                    result.append(variable)
-        return result
-        #return [variable for path in self.paths for variable in path.variables()]
-
-    def variable_names(self, bound_vars=None):
-        return [variable.strip('?') for variable in self.variables(bound_vars)]
-
-    def to_triples_str(self, **bindings):
-        triples = '\n'.join(statement for path in self.paths for statement in path.to_triple_list())
-        for variable, value in bindings.items():
-            assert isinstance(value, Node), value
-            triples = triples.replace('?{} '.format(variable), '{} '.format(value))
-        return triples
-
-    def to_triples_list(self, **bindings):
-        return self.to_triples_str(**bindings).splitlines()
-
-    def to_select(self, *constraints, **bindings):
-        template = dedent('''
-            SELECT {variables}
-            WHERE {{
-            {triples}
-            }}
-            {constraints}
-        ''').strip()
-        variables = self.variables()
-        for bound in bindings:
-            if '?' + bound in variables:
-                variables.remove('?' + bound)
-        return template.format(
-            variables=' '.join(variables),
-            triples=indent(self.to_triples_str(**bindings), '    '),
-            constraints='\n'.join(constraints)
-        )
-
-    def as_prepared_query(self, *constraints, **bindings):
-        return prepareQuery(self.to_select(*constraints, **bindings))
+class URI: 
+    PREFIXES = { 
+        '_': '_', 
+        'db': 'http://dbpedia.org/', 
+        'dbo': 'http://dbpedia.org/ontology/', 
+        'dbp': 'http://dbpedia.org/property/', 
+        'dbr': 'http://dbpedia.org/resource/', 
+        'dc': 'http://purl.org/dc/elements/1.1/', 
+        'foaf': 'http://xmlns.com/foaf/0.1/', 
+        'owl': 'http://www.w3.org/2002/07/owl#', 
+        'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#', 
+        'rdfs': 'http://www.w3.org/2000/01/rdf-schema#', 
+        'res': 'http://www.w3.org/2005/sparql-results#', 
+        'skos': 'http://www.w3.org/2004/02/skos/core#', 
+        'xsd': 'http://www.w3.org/2001/XMLSchema#', 
+        'umbel-rc': 'http://umbel.org/umbel/rc/', 
+        'umbel': 'http://umbel.org/umbel#', 
+    } 
+    REVERSED = sorted( 
+        ([namespace, prefix] for prefix, namespace in PREFIXES.items()), 
+        key=(lambda kv: -len(kv[0])), 
+    ) 
+ 
+    def __init__(self, uri, prefix=None): 
+        if prefix: 
+            self.prefix = prefix 
+            self.fragment = uri 
+            self.uri = URI.PREFIXES[prefix] + uri 
+        else: 
+            self.uri = uri 
+            for namespace, alias in URI.REVERSED: 
+                if uri.startswith(namespace): 
+                    self.prefix = alias 
+                    self.fragment = uri[len(namespace):] 
+ 
+    def __str__(self): 
+        return '<' + self.uri + '>' 
+ 
+    @property 
+    def short_str(self): 
+        return self.prefix + ':' + self.fragment 
 
 
 def create_sqlite_graph(path, create=True, identifier=None):
@@ -232,36 +88,6 @@ class KnowledgeSource:
             list[dict[str:str]]: A list of variable bindings.
         """
         raise NotImplementedError
-
-    def query(self, query, *constraints, **bindings):
-        results = self.query_sparql(query.to_select(*constraints, **bindings))
-        result_graph = Graph()
-        result_graph.parse(data=results, format='n3')
-        variable_paths = []
-        for variable in query.variable_names():
-            variable = variable.strip('?')
-            variable_paths.append(
-                V(
-                    'solution',
-                    res__binding=V(
-                        variable + '_binding',
-                        res__variable=L(variable),
-                        res__value=V(variable)
-                    ),
-                )
-            )
-        res_qry = Query(V('root', res__solution=V('solution')), *variable_paths)
-        triples = set()
-        for values in result_graph.query(res_qry.to_select()):
-            val_dict = dict(zip(res_qry.variable_names(), values))
-            bindings = {}
-            for variable, value in val_dict.items():
-                value = value.n3()
-                bindings[variable] = Node.from_str(value)
-            triples |= set(query.to_triples_str(**bindings).split('\n'))
-        triples = '\n'.join(sorted(triples))
-        #self.graph.parse(data=prefixes + '\n' + triples, format='n3')
-        return triples
 
 
 class KnowledgeFile(KnowledgeSource):
@@ -302,19 +128,6 @@ class KnowledgeFile(KnowledgeSource):
             results.append({str(variable):str(uri) for variable, uri in result.items()})
         return results
 
-    def query(self, query, *constraints, **bindings):
-        results = self.query_sparql(query.to_select(*constraints, **bindings))
-        variables = query.variable_names(list(bindings.keys()))
-        triples = set()
-        for row in results:
-            val_dict = dict(zip(variables, row))
-            result_bindings = {}
-            for variable, value in val_dict.items():
-                result_bindings[variable] = Node.from_str(value.n3())
-            triples |= set(query.to_triples_str(**bindings, **result_bindings).split('\n'))
-        triples = '\n'.join(sorted(triples))
-        return triples
-
 
 class SparqlEndpoint(KnowledgeSource):
 
@@ -327,6 +140,3 @@ class SparqlEndpoint(KnowledgeSource):
         for bindings in self.endpoint.query().bindings:
             results.append({key:value.value for key, value in bindings.items()})
         return results
-
-    def query(self, query, *constraints, **bindings):
-        return self.query_sparql(query.to_select(*constraints, **bindings))
