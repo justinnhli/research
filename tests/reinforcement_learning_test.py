fcbff35a500f63d4796c53acbbd6003117cce6ee
Justin Li <justinnhli@gmail.com>
2018-05-21 19:14:24 -0700

    test Q-learning agent

--- tests/reinforcement_learning_test.py
+++ tests/reinforcement_learning_test.py
@@ -12,6 +12,8 @@ sys.path.insert(0, dirname(DIRECTORY))
 from research.reinforcement_learning import State, Action
 from research.reinforcement_learning import GridWorld, SimpleTMaze
 from research.reinforcement_learning import gating_memory
+from research.reinforcement_learning import TabularQLearningAgent
+from research.reinforcement_learning import epsilon_greedy
 
 RLTestStep = namedtuple('RLTestStep', ['observation', 'actions', 'action', 'reward'])
 
@@ -137,3 +139,28 @@ def test_simpletmaze_gatingmemory():
         if expected.action is not None:
             reward = env.react(expected.action)
             assert reward == expected.reward
+
+
+def test_agent():
+    """Test the epsilon greedy tabular Q-learning agent."""
+    env = GridWorld(
+        width=3,
+        height=3,
+        start=[0, 0],
+        goal=[2, 2],
+    )
+    agent = epsilon_greedy(TabularQLearningAgent, 0.05)(0.1, 0.9)
+    for _ in range(1000):
+        env.new_episode()
+        reward = None
+        while env.get_actions() != []:
+            observation = env.get_observation()
+            actions = env.get_actions()
+            reward = env.react(agent.act(observation, actions, reward))
+        observation = env.get_observation()
+        actions = env.get_actions()
+        agent.act(observation, actions, reward)
+    for row in range(3):
+        for col in range(3):
+            best_action = agent.get_best_action(State(row=row, col=col))
+            assert best_action is None or best_action.name in ['down', 'right']
