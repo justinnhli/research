75adbe8addd9f6ca23dc367fa522ef6d52fe8db3
Justin Li <justinnhli@gmail.com>
2018-06-28 17:27:05 -0700

    add RandomClass

---
+++ research/randomclass.py
@@ -0,0 +1,17 @@
+"""Encapsulate a randomized object."""
+
+from random import Random, random
+
+class RandomClass:
+    """Encapsulation of a randomized object."""
+
+    def __init__(self, random_seed=None):
+        """Initialize the RandomClass.
+
+        Arguments:
+            random_seed (Object): The random seed.
+        """
+        if random_seed is None:
+            random_seed = random()
+        self.random_seed = random_seed
+        self.rng = Random(self.random_seed)
