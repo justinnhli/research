c0594323b644bdad757bee05b391f96e19a37e88
Justin Li <justinnhli@gmail.com>
2018-07-02 16:52:44 -0700

    add UnionFind

---
+++ research/data_structures.py
@@ -0,0 +1,79 @@
+"""Utility data structures."""
+
+class UnionFind:
+    """UnionFind for discrete sets."""
+
+    def __init__(self, nodes=None):
+        """Initialize the UnionFind.
+
+        Arguments:
+            nodes (Hashable): The object to be hashed.
+        """
+        if nodes is None:
+            nodes = []
+        self.parents = {node: node for node in nodes}
+
+    def __len__(self):
+        return len(self.parents)
+
+    def __contains__(self, node):
+        return node in self.parents
+
+    def __getitem__(self, node):
+        path = []
+        while self.parents[node] != node:
+            path.append(node)
+            node = self.parents[node]
+        path.append(node)
+        rep = path[-1]
+        for step in path:
+            self.parents[step] = rep
+        return rep
+
+    def __iter__(self):
+        return iter(self.parents)
+
+    def __bool__(self):
+        return True if self.parents else False
+
+    def union(self, node1, node2):
+        """Join two discrete sets.
+
+        Arguments:
+            node1 (Hashable): A member of one set to be joined.
+            node2 (Hashable): A member of the other set to be joined.
+        """
+        self.add(node1)
+        rep1 = self[node1]
+        self.add(node2)
+        rep2 = self[node2]
+        self.parents[rep2] = rep1
+
+    def same(self, node1, node2):
+        """Check if two members are in the same set.
+
+        Arguments:
+            node1 (Hashable): A member of one set.
+            node2 (Hashable): A member of the other set.
+
+        Returns:
+            bool: True if the members are in the same set.
+        """
+        return self[node1] == self[node2]
+
+    def add(self, node, parent=None):
+        """Add a node.
+
+        Arguments:
+            node (Hashable): The member to add.
+            parent (Hashable): A node from the same set, if any.
+
+        Returns:
+            bool: True if a new member has been added.
+        """
+        if node in self.parents:
+            return False
+        if parent is None:
+            parent = node
+        self.parents[node] = parent
+        return True
