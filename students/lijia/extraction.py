381000de4d296456abaaf7a5761e16c19e1f27b8
Unknown <lil@oxy.edu>
2018-07-14 21:53:03 -0700

    delete test in extraction

--- students/lijia/extraction.py
+++ students/lijia/extraction.py
@@ -24,7 +24,6 @@ NP_DIR = join_path(OUTPUT_DIR, "np")
 VO_DIR = join_path(OUTPUT_DIR, "vpo")
 
 # SVO = namedtuple('SVO', ('subject', 'verb', 'preposition', 'object'))
-TestCase = namedtuple('TestCase', ['sentence', 'expected_output'])
 VPO = namedtuple('VPO', ('verb', 'prep', 'object'))
 NP = namedtuple('NP', ['noun', 'adjectives'])
 
@@ -152,47 +151,6 @@ def search_file(filename, string):
             if string == ls[0]:
                 results[ls[1]] = float(ls[2])
     return results
-
-
-def test_extract_vpo():
-    """ test cases for extracting vpo"""
-    test_cases = [
-        TestCase(
-            "Jack hits me with ball",
-            [VPO(verb='hit', prep='with', object='ball')],
-        ),
-        TestCase(
-            "Jonney enjoys sewing the little blanket with needle and thread",
-            [VPO(verb='sew', prep=None, object='blanket'), VPO(verb='sew', prep='with', object='needle'),
-             VPO(verb='sew', prep='with', object='thread')]
-        ),
-    ]
-    for test_case in test_cases:
-        message = [
-            "Parsing sentence: " + test_case.sentence,
-            "    but failed to see expected result: " + str(test_case.expected_output),
-        ]
-        assert test_case.expected_output == extract_vpo(nlp(test_case.sentence)), "\n".join(message)
-
-
-def test_extract_np():
-    """ test cases for extracting vpo"""
-    test_cases = [
-        TestCase(
-            "The tall and skinny girl kiss Jack",
-            [NP(noun='girl', adjectives=['tall', 'skinny'])],
-        ),
-        TestCase(
-            "The girl is tall and skinny.",
-            [NP(noun='girl', adjectives=['tall', 'skinny'])],
-        ),
-    ]
-    for test_case in test_cases:
-        message = [
-            "Parsing sentence: " + test_case.sentence,
-            "    but failed to see expected result: " + str(test_case.expected_output),
-        ]
-        assert test_case.expected_output == extract_np(nlp(test_case.sentence)), "\n".join(message)
 
 
 def extract_vpo(doc):
@@ -452,7 +410,6 @@ def pipe():
         overall(word)
 
 
-
 def main():  # pylint: disable= missing-docstring
     start = time.time()
     pipe()
@@ -461,7 +418,4 @@ def main():  # pylint: disable= missing-docstring
 
 
 if __name__ == '__main__':
-    test_extract_vpo()
-    test_extract_np()
-    print("passed all test cases")
     main()
