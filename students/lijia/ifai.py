67dc3514cd69810bdb1c5a038a6699fb851715c7
Unknown <lil@oxy.edu>
2018-07-17 00:08:47 -0700

    add interface function

--- students/lijia/ifai.py
+++ students/lijia/ifai.py
@@ -1,15 +1,32 @@
-
-from students.lijia.word2vec import *
-from students.lijia.extraction import *    # todo: rename extraction.py
+import sys
+from os.path import dirname, realpath
 
 ROOT_DIRECTORY = dirname(dirname(dirname(realpath(__file__))))
 sys.path.insert(0, ROOT_DIRECTORY)
 
-
-GOOGLE_NEWS_MODEL_PATH = join_path(ROOT_DIRECTORY, 'data/models/GoogleNews-vectors-negative300.bin')
-model = load_model(GOOGLE_NEWS_MODEL_PATH)
+from students.lijia.utils import get_manipulable_noun
+from students.lijia.word2vec import get_verbs_for_noun as w2v_get_verbs_for_noun
+from students.lijia.extraction import get_verbs_for_noun as prob_get_verbs_for_noun
+# todo: rename extraction.py
 
 
 def get_action(sentence):
-    word2vec_actions = possible_actions(model, sentence)
-    probability_actions = get_action_for_text(sentence)
+    verb_dict = {}
+    actions = []
+    nouns = get_manipulable_noun(sentence)
+    print(nouns)
+    for noun in nouns:
+        prob_verbs = prob_get_verbs_for_noun(noun)
+        word2vec_verbs = w2v_get_verbs_for_noun(noun)
+        verb_dict[noun] = prob_verbs + word2vec_verbs
+        actions.extend(["%s %s" % (verb, noun) for verb in verb_dict[noun]])
+    return actions
+
+
+def main():
+    sentence = "She loves the kitten that was thrown away by her Dad. She carve down the cat with paper and knife."
+    print(get_action(sentence))
+
+if __name__ == '__main__':
+    main()
+
