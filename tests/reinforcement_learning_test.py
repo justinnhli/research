f62fdfeec516fa55ed15ac1d01707bbaf333746d
Justin Li <justinnhli@gmail.com>
2019-02-01 20:25:35 -1000

    fix RL tests

--- tests/reinforcement_learning_test.py
+++ tests/reinforcement_learning_test.py
@@ -15,7 +15,6 @@ from research.rl_environments import GridWorld, SimpleTMaze
 from research.rl_environments import gating_memory, fixed_long_term_memory, memory_architecture
 from research.rl_agents import TabularQLearningAgent
 from research.rl_agents import epsilon_greedy
-from research.randommixin import RandomMixin
 
 RLTestStep = namedtuple('RLTestStep', ['observation', 'actions', 'action', 'reward'])
 
@@ -31,10 +30,8 @@ def test_gridworld():
     env.start_new_episode()
     expected_steps = [
         RLTestStep(State(row=0, col=0), [Action('down'), Action('right')], Action('right'), -1),
-        RLTestStep(State(row=0, col=1), [Action('down'), Action('left')], Action('right'), -1),
         RLTestStep(State(row=0, col=1), [Action('down'), Action('left')], Action('down'), -1),
         RLTestStep(State(row=1, col=1), [Action('up'), Action('down'), Action('left')], Action('down'), -1),
-        RLTestStep(State(row=2, col=1), [Action('up'), Action('left')], Action('down'), -1),
         RLTestStep(State(row=2, col=1), [Action('up'), Action('left')], Action('up'), -1),
         RLTestStep(State(row=1, col=1), [Action('up'), Action('down'), Action('left')], Action('left'), -1),
         RLTestStep(State(row=1, col=0), [Action('up'), Action('down'), Action('right')], Action('down'), 1),
@@ -260,7 +257,6 @@ def test_agent():
         for col in range(3):
             best_action = agent.get_best_stored_action(State(row=row, col=col))
             assert best_action is None or best_action.name in ['down', 'right']
-    agent.print_policy()
     # the optimal policy takes 8 steps for a 5x5 grid
     # -6 comes from 7 steps of -1 reward and 1 step of +1 reward
     assert returns[-1] == -6
@@ -269,41 +265,44 @@ def test_agent():
 def test_memory_architecture():
     """Test the memory architecture meta-environment."""
 
-    class TestEnv(Environment, RandomMixin):
+    class TestEnv(Environment):
         """A simple environment with a single string state."""
 
-        def __init__(self, string=''):
+        def __init__(self, size, index=0):
             """Initialize the TestEnv.
 
             Arguments:
-                string (str): The initial string.
+                size (int): The length of one side of the square.
+                index (int): The initial int.
             """
-            self.init_string = string
-            self.string = self.init_string
             super().__init__()
+            self.size = size
+            self.init_index = index
+            self.index = self.init_index
 
         def get_state(self): # noqa: D102
-            return self.get_observation()
+            return State(index=self.index)
 
         def get_observation(self): # noqa: D102
-            return State(string=self.string)
+            return State(index=self.index)
 
         def get_actions(self): # noqa: D102
-            if self.string == 'done':
+            if self.index == -1:
                 return []
             else:
-                return [Action('no-op')]
+                return [Action(str(i)) for i in range(-1, size * size)]
 
         def reset(self): # noqa: D102
             self.start_new_episode()
 
         def start_new_episode(self): # noqa: D102
-            self.string = self.init_string
+            self.index = self.init_index
 
         def react(self, action): # noqa: D102
+            assert action in self.get_actions()
             if action.name != 'no-op':
-                self.string = action.name
-            if self.string == 'done':
+                self.index = int(action.name)
+            if self.end_of_episode():
                 return 100
             else:
                 return -1
@@ -311,58 +310,47 @@ def test_memory_architecture():
         def visualize(self): # noqa: D102
             pass
 
-    env = memory_architecture(TestEnv)(explicit_actions=False)
-    env.start_new_episode()
     size = 5
+    env = memory_architecture(TestEnv)(size=size, index=0)
+    env.start_new_episode()
     for i in range(size * size):
         env.add_to_ltm(index=i, row=(i // size), col=(i % size))
     # test observation
     assert env.get_observation() == State(
-        perceptual_string='',
-        action_name='no-op',
+        perceptual_index=0,
     ), env.get_observation()
     # test actions
     assert (
-        set(env.get_actions()) == set(
-            [
-                Action("copy", dst_attr='string', dst_buf='query', src_attr='string', src_buf='perceptual'),
-                Action("copy", dst_attr='string', dst_buf='action', src_attr='string', src_buf='perceptual'),
-                Action("copy", dst_attr='name', dst_buf='action', src_attr='string', src_buf='perceptual'),
-            ]
-        )
+        set(env.get_actions()) == set([
+                *(Action(str(i)) for i in range(-1, size * size)),
+                Action('copy', src_buf='perceptual', src_attr='index', dst_buf='query', dst_attr='index'),
+        ])
     ), set(env.get_actions())
     # test pass-through reaction
-    reward = env.react(Action('write', buf='action', attr='name', val='test-action'))
+    reward = env.react(Action('9'))
     assert env.get_observation() == State(
-        perceptual_string='test-action',
-        action_name='no-op',
+        perceptual_index=9,
     ), env.get_observation()
     assert reward == -1, reward
     # query test
-    env.react(Action('write', buf='query', attr='index', val=9))
+    env.react(Action('copy', src_buf='perceptual', src_attr='index', dst_buf='query', dst_attr='index'))
     assert env.get_observation() == State(
-        perceptual_string='test-action',
-        action_name='no-op',
+        perceptual_index=9,
         query_index=9,
         retrieval_index=9,
         retrieval_row=1,
         retrieval_col=4,
     ), env.get_observation()
     # query with no results
-    env.react(Action('write', buf='query', attr='row', val=4))
+    env.react(Action('copy', src_buf='retrieval', src_attr='row', dst_buf='query', dst_attr='row'))
+    reward = env.react(Action('0'))
+    env.react(Action('copy', src_buf='perceptual', src_attr='index', dst_buf='query', dst_attr='index'))
     assert env.get_observation() == State(
-        perceptual_string='test-action',
-        action_name='no-op',
-        query_index=9,
-        query_row=4,
-    ), env.get_observation()
-    # query and retrieval should clear on external action
-    env.react(Action('write', buf='action', attr='name', val='clear-memory-buffer'))
-    assert env.get_observation() == State(
-        perceptual_string='clear-memory-buffer',
-        action_name='no-op',
+        perceptual_index=0,
+        query_index=0,
+        query_row=1,
     ), env.get_observation()
     # complete the environment
-    reward = env.react(Action('write', buf='action', attr='name', val='done'))
+    reward = env.react(Action('-1'))
     assert env.end_of_episode()
     assert reward == 100, reward
