77e91318da632230f1a28c5879cb77c8c6dc8bda
Justin Li <justinnhli@gmail.com>
2019-02-13 13:23:04 -0800

    create files for individual experiments
    .

--- projects/ijcai/exp-query-single.py
+++ projects/ijcai/exp-query-single.py
@@ -34,7 +34,7 @@ def testing():
     env = memory_architecture(RecordStore)(
         # record store
         schema=SCHEMAS['title_year'],
-        num_albums=3,
+        num_albums=1000,
         # memory architecture
         max_internal_actions=5,
         knowledge_store=SparqlKB(
@@ -95,7 +95,7 @@ def run_experiment(params):
     )
     env = memory_architecture(RecordStore)(
         # record store
-        schema=params.schema,
+        schema=SCHEMAS[params.schema_name],
         num_albums=params.num_albums,
         # memory architecture
         max_internal_actions=params.max_internal_actions,
@@ -113,54 +113,26 @@ def run_experiment(params):
         eval_frequency=params.eval_frequency,
         min_return=-100,
     )
-    filename = '-'.join([
-        f'schema{params.schema_name}',
-        f'seed{params.random_seed}',
-        f'albums{params.num_albums}',
-    ])
     episodes = range(0, params.num_episodes, params.eval_frequency)
-    results_path = Path(DIRECTORY, 'data', 'exp2')
-    results_path.mkdir(parents=True, exist_ok=True)
+    data_file = Path(DIRECTORY, 'data', params.schema_name, f'seed{params.random_seed}')
+    data_file.parent.mkdir(parents=True, exist_ok=True)
     for episode, mean_return in zip(episodes, trial_result):
-        with results_path.joinpath(filename).open('a') as fd:
+        with data_file.open('a') as fd:
             fd.write(f'{datetime.now().isoformat("_")} {episode} {mean_return}\n')
-        if (episode + params.eval_frequency) % 1000 == 0:
-            with results_path.joinpath(filename).open('a') as fd:
-                fd.write(30 * '-' + '\n')
-                visited = set()
-                env.start_new_episode()
-                for step in range(10):
-                    fd.write(f'{step}\n')
-                    observation = env.get_observation()
-                    fd.write(f'{observation}\n')
-                    if observation in visited:
-                        fd.write('\n')
-                        fd.write('Looped; quitting.\n')
-                        break
-                    elif env.end_of_episode():
-                        break
-                    visited.add(observation)
-                    fd.write(f'{feature_extractor(observation)}\n')
-                    action = agent.get_best_stored_action(env.get_observation(), actions=env.get_actions())
-                    fd.write(f'{action}\n')
-                    env.react(action)
-                    fd.write('\n')
-                fd.write(30 * '-' + '\n')
 
 
 def main():
     pspace = PermutationSpace(
-        ['schema_name', 'random_seed', 'num_albums'],
+        ['random_seed',],
         random_seed=[
             0.35746869278354254, 0.7368915891545381, 0.03439267552305503, 0.21913569678035283, 0.0664623502695384,
             #0.53305059438797, 0.7405341747379695, 0.29303361447547216, 0.014835598224628765, 0.5731489218909421,
         ],
         num_episodes=10000,
         eval_frequency=100,
-        num_albums=[1000,],
+        num_albums=1000,
         max_internal_actions=5,
-        schema_name=['title_year', 'title_genre_decade', 'title_country',],
-        schema=(lambda schema_name: SCHEMAS[schema_name]),
+        schema_name='title_year',
     )
     size = len(pspace)
     for i, params in enumerate(pspace, start=1):
