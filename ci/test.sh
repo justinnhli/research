d796a023549c72e18a07dd61a665cf66b9b25601
Justin Li <justinnhli@gmail.com>
2019-03-06 14:07:50 -0800

    include branch coverage in report

--- ci/test.sh
+++ ci/test.sh
@@ -15,4 +15,4 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # go up once to the project root directory
 cd ..
 # run pytest
-"$pytest" --verbose --cov=research/ tests
+"$pytest" --verbose --cov=research/ --cov-config .coveragerc tests
