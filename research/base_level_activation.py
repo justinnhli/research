cc7cb70a4e6efcc4d5cab7a2093f9fa5028a1563
Lily-Gebhart <102749415+Lily-Gebhart@users.noreply.github.com>
2023-04-26 17:25:38 -0700

    Update and rename BaseLevelActivation.py to base_level_activation.py

--- research/base_level_activation.py
+++ research/base_level_activation.py
@@ -1,21 +1,23 @@
-import math
 from research import ActivationDynamics
 from collections import defaultdict
+import math
 
 
 class BaseLevelActivation(ActivationDynamics):
     """Activation functions that calculate the base level activation of objects"""
-    constant_offset = 0;
-    activation_base = 2;
-    decay_parameter = 0.01;
 
-    def __init__(self, ltm, **kwargs):
+    def __init__(self, ltm, constant_offset=0, activation_base=2, decay_parameter=0.01, **kwargs):
         """Initialize the ActivationDynamics.
         Parameters:
             ltm (LongTermMemory): The LongTermMemory that will be using this activation.
         """
         super().__init__(ltm, **kwargs)
         self.activations = defaultdict(lambda:list())
+        self.constant_offset = constant_offset
+        self.activation_base = activation_base
+        self.decay_parameter = decay_parameter
+
+
 
 
     def activate(self, mem_id, time):
@@ -25,6 +27,7 @@ class BaseLevelActivation(ActivationDynamics):
             time (int): The time of activation. Optional.
         """
         self.activations[mem_id].append([time, 1])
+
         curr_act_candidates = list(self.ltm.knowledge.get(mem_id))
         next_act_candidates = list(self.ltm.knowledge.get(mem_id))
         graph_units = 1
@@ -46,6 +49,7 @@ class BaseLevelActivation(ActivationDynamics):
 
 
 
+
     def get_activation(self, mem_id, time):
         """Get the activation of the element with the given ID.
         Parameters:
@@ -58,12 +62,16 @@ class BaseLevelActivation(ActivationDynamics):
         if act_times_list == [] or (len(act_times_list) == 1 and act_times_list[0][0] == 0):
             return None
 
-        time_since_last_act_list = [[time - time_spreading_pair[0], time_spreading_pair[1]] for time_spreading_pair in act_times_list]
+        time_since_last_act_list = [[time - time_spreading_pair[0], time_spreading_pair[1]] for time_spreading_pair
+                                    in act_times_list]
         base_act_sum_term = 0
         for retrieval_pair in range(len(time_since_last_act_list)):
             if (act_times_list[retrieval_pair][0] > 0):
-                base_act_sum_term = time_since_last_act_list[retrieval_pair][1]*(time_since_last_act_list[retrieval_pair][0] ** (-self.decay_parameter)) + base_act_sum_term
-        base_level_activation = self.constant_offset + math.log(base_act_sum_term)
+                base_act_sum_term = time_since_last_act_list[retrieval_pair][1]*(
+                        time_since_last_act_list[retrieval_pair][0] ** (-self.decay_parameter)) + base_act_sum_term
 
+        base_level_activation = self.constant_offset + math.log(base_act_sum_term)
         return base_level_activation
+
+
 
